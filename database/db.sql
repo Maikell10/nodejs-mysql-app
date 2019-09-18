acbc327c08c2d358ac89e3a20ef462e6d158e883
Maikell <maikell.ods10@gmail.com>
2019-09-18 12:00:47 -0400

    fixed vulnerability

--- database/db.sql
+++ database/db.sql
@@ -4,7 +4,7 @@ USE database_links;
 
 -- USERS TABLE
 CREATE TABLE users(
-    id INT(11) NOT NULL,
+    id INT(11) NOT NULL IDENTITY(1,1),
     username VARCHAR(16) NOT NULL,
     password VARCHAR(60) NOT NULL,
     fullname VARCHAR(100) NOT NULL
@@ -13,14 +13,10 @@ CREATE TABLE users(
 ALTER TABLE users
     ADD PRIMARY KEY (id);
 
-ALTER TABLE users
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
-
-DESCRIBE users;
 
 --LINKS TABLE
 CREATE TABLE links(
-    id INT(11) NOT NULL,
+    id INT(11) NOT NULL IDENTITY(1,1),
     title VARCHAR(150) not null,
     url VARCHAR(255) NOT NULL,
     description TEXT,
@@ -31,8 +27,3 @@ CREATE TABLE links(
 
 ALTER TABLE links
     ADD PRIMARY KEY (id);
-
-ALTER TABLE links
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
-
-DESCRIBE links;
